d3b320663f4bed14af79d5caa1c01f5380404e76
schio <ghsehr1@gmail.com>
2019-02-04 21:46:11 +0900

    got it reason of bug
    피드 개수 최대치를 설정해주고 최대치만큼 피드 긁어옴

--- fullyinfofeed.py
+++ fullyinfofeed.py
@@ -3,8 +3,14 @@ from pprint import pprint as p
 import driver
 from multiprocessing import Pool
 import time
+import numpy as np
 import re
 from selenium.webdriver.common.keys import Keys
+
+def unique_rows(a):
+    a = np.ascontiguousarray(a)
+    unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
+    return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
 
 def outerInfo(hashtag, no_of_pagedowns):
     # 인스타그램 공식 홈페이지
@@ -12,6 +18,9 @@ def outerInfo(hashtag, no_of_pagedowns):
     
     # 긁어올 주소
     url = 'https://www.instagram.com/explore/tags/'+hashtag
+
+    #인스타그램 정보 담을 리스트 선언
+    fullyinfofeed=[]
 
     # 크롬 드라이버 생성
     chromeDriverPath = '/home/sco/install/chrome/chromedriver'
@@ -21,35 +30,36 @@ def outerInfo(hashtag, no_of_pagedowns):
     # 접속 URL의 body 부분 볼 것이다
     elem = browser.find_element_by_tag_name("body")
 
+    beforeNumOfFeed=0
+    afterNumOfFeed=1
     # URL 접속 후 PAGE DOWN 키 눌러(스크롤 내려서) 피드를 추가 로드
-    while no_of_pagedowns:
+    while beforeNumOfFeed != 42:
+        beforeNumOfFeed = afterNumOfFeed
         elem.send_keys(Keys.PAGE_DOWN) # PAGE DOWN Key 누름
-        elem.send_keys(Keys.PAGE_DOWN) # PAGE DOWN Key 누름
-        elem.send_keys(Keys.PAGE_DOWN) # PAGE DOWN Key 누름
+        elem.send_keys(Keys.PAGE_DOWN)
         time.sleep(0.5) # 누른 후 정보 불러오기 위한 대기시간 부여
         no_of_pagedowns-=1 
 
-    # 지금까지 로드된 html 파싱
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    # 파싱한 html에서 피드 관련 정보가 담긴 v1Nh3 kIKUG _bz0w div만 볼 것
-    divs=soup.find_all("div", attrs={'class':'v1Nh3 kIKUG _bz0w'}) 
-    
-    #인스타그램 정보 담을 리스트 선언
-    fullyinfofeed=[]
-    
-    # html에서 정보추출을 위한 정규표현식 선언 
-    feedLinkRe=re.compile(r"/p/.{11}")
-    tagRe=re.compile(r"img alt=\".*class=\"F")
+        # 지금까지 로드된 html 파싱
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        # 파싱한 html에서 피드 관련 정보가 담긴 v1Nh3 kIKUG _bz0w div만 볼 것
+        divs=soup.find_all("div", attrs={'class':'v1Nh3 kIKUG _bz0w'}) 
+               
+        # html에서 정보추출을 위한 정규표현식 선언 
+        feedLinkRe=re.compile(r"/p/.{11}")
+        tagRe=re.compile(r"img alt=\".*class=\"F")
 
-    for div in divs:
-        div=str(div.select('a')) # instagram feed link
-        tag=str(tagRe.findall(div))[11:-12]
-        fullyinfofeed.append(\
-            [instagramHompage+feedLinkRe.findall(div)[0],\
-            tag\
-            ])
+        for div in divs:
+            div=str(div.select('a')) # instagram feed link
+            tag=str(tagRe.findall(div))[11:-12]
+            fullyinfofeed.append(\
+                [instagramHompage+feedLinkRe.findall(div)[0],\
+                tag\
+                ])
+
+        afterNumOfFeed = len(unique_rows(fullyinfofeed))
 
     return fullyinfofeed
     # p(fullyinfofeed)
@@ -69,10 +79,19 @@ def outerInfo(hashtag, no_of_pagedowns):
 # tag.has_attr('id')
 # 있으면 True, 없으면 False
 
-for i in range(40):
-    start_time = time.time()
-    test=outerInfo('빅데이터',i)
-    print("Num Of Pagedown", i,"\t: %d seconds" % (time.time() - start_time),"| Num of feed :",len(test))
+# for i in range(1,4):
+#     start_time = time.time()
+#     test=outerInfo('호에에에에엥',i)
+#     b = unique_rows(test)
+#     print("Num Of Pagedown", i,"\t: %d seconds" % (time.time() - start_time),"| Num of feed :",len(b))
+
+start_time = time.time()
+test=outerInfo('호에에에에엥',0)
+b = unique_rows(test)
+print("%d seconds" % (time.time() - start_time),"| Num of feed :",len(b))
+
+
+
 # pool = Pool(processes=4) # 4개의 프로세스를 사용합니다.
 # pool.map(useHashTag('빅데이터')) # get_contetn 함수를 넣어줍시다.
 
