de3dd062cecc3ad0960b1ef6f9bbf39b2718bdd2
schio <ghsehr1@gmail.com>
2019-02-19 17:11:43 +0900

    Update driver.py

--- driver.py
+++ driver.py
@@ -8,7 +8,7 @@ def createDriver(chromeDriverPath, targetUrl):
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-extensions")
-    #for head less mode detection
+    #for anti head less mode detection
     options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36") # agent change 
     options.add_argument("lang=ko_KR") 
 
@@ -16,7 +16,7 @@ def createDriver(chromeDriverPath, targetUrl):
     driver = webdriver.Chrome(chrome_options=options, executable_path=chromeDriverPath)
     driver.get(targetUrl)
 
-    # for head less mode detection
+    # for anti head less mode detection
     driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});") #num of plugin spoofing
     driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
     driver.execute_script("const getParameter = WebGLRenderingContext.getParameter;WebGLRenderingContext.prototype.getParameter = function(parameter) {if (parameter === 37445) {return 'NVIDIA Corporation'} if (parameter === 37446) {return 'NVIDIA GeForce GTX 980 Ti OpenGL Engine';}return getParameter(parameter);};")
